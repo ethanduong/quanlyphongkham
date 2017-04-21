de54b1db72fd6d027f6c7db07486c35fc95dd2bc
letiencong96 <letiencong96@gmail.com>
2017-04-21 23:36:28 +0700

    checklogin

--- PhongKhamDaKhoa/DanhMucThongSoKyThuat.aspx.cs
+++ PhongKhamDaKhoa/DanhMucThongSoKyThuat.aspx.cs
@@ -16,6 +16,14 @@ namespace PhongKhamDaKhoa
         private int _stt;
         protected void Page_Load(object sender, EventArgs e)
         {
+            clsUser_entity objUser = new clsUser_entity();
+            clsUser_controller con = new clsUser_controller();
+            if (QLPHONGKHAM.Common.GetUserID() < 0)
+            {
+                //Response.Write("<script>alert('Bạn chưa đăng nhập. Vui lòng đăng nhập')</script>");
+
+                ClientScript.RegisterStartupScript(this.GetType(), "Log", "<script type='text/javascript'>alert('Bạn chưa đăng nhập, Vui lòng đăng nhập');window.location='Login.aspx';</script>'");
+            }
             if (!IsPostBack)
             {
                 LoadDataToGridView();
